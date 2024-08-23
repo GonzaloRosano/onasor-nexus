872890743f5f3aad57ad56015ef6f3d264291e65
GonzaloRosano <rosanogonzalo@gmail.com>
2024-08-23 09:27:40 -0300

    Cambio vercel fix and update home text

--- src/main.tsx
+++ src/main.tsx
@@ -41,7 +41,6 @@ createRoot(document.getElementById('root')!).render(
     <I18nextProvider i18n={i18next}>
       <Router>
         <Header />
-
         <App />
       </Router>
     </I18nextProvider>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,7 @@
 export const Home = () => {
   return (
     <main className="text-white/90 container mx-auto  px-4 py-8">
-      <p className="mt-4 text-lg">
-        Somos ArefyAliance, un equipo dedicado a dominar el mundo de Minecraft.
-        Conoce a nuestros miembros y descubre nuestras épicas aventuras y logros
-        dentro del servidor.
-      </p>
+      <p className="mt-4 text-lg"></p>
     </main>
   );
 };
