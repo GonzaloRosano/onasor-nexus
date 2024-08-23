55b5f0aceee30b7996891e21eedcd17f5144669d
GonzaloRosano <rosanogonzalo@gmail.com>
2024-08-23 09:33:18 -0300

    Fix home first-paragraph

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
-import { t } from 'i18next';
+import { useTranslation } from 'react-i18next';
 
 export const Home = () => {
+  const { t } = useTranslation('global');
+
   return (
     <main className="text-white/90 container mx-auto  px-4 py-8">
       <p className="mt-4 text-lg">{t('home.first-paragraph')}</p>
