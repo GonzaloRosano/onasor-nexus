72debd6543cf5b4ca034907e67a1576c1a9703f7
GonzaloRosano <rosanogonzalo@gmail.com>
2024-08-23 09:31:18 -0300

    Fix Home and vercel routes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import { t } from 'i18next';
+
 export const Home = () => {
   return (
     <main className="text-white/90 container mx-auto  px-4 py-8">
-      <p className="mt-4 text-lg"></p>
+      <p className="mt-4 text-lg">{t('home.first-paragraph')}</p>
     </main>
   );
 };
